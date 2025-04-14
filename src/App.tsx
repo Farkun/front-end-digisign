d883ee60387546b5fe6116cb91d6f714f0517149
lafathalfath <iqnaraidan12@gmail.com>
2025-04-14 17:03:15 +0700

    UPDATE: integrate encrypt decrypt and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Login from "./pages/login";
