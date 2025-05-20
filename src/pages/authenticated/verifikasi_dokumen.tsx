f7050c9ed69bef0dd374423a2984c62db2f5cf70
lafathalfath <iqnaraidan12@gmail.com>
2025-05-20 15:03:05 +0700

    `Refactor verifikasi_dokumen.tsx to display multiple documents`

--- src/pages/authenticated/verifikasi_dokumen.tsx
+++ src/pages/authenticated/verifikasi_dokumen.tsx
@@ -6,7 +6,7 @@ import Cookies from 'universal-cookie';
 
 const VerifikasiDokumen = (): ReactElement => {
 
-    const [result, setResult] = useState<string | null>(null)
+    const [result, setResult] = useState<any[]>([])
 
     const handleChange = async (e: any): Promise<void> => {
         const file = e.target.files[0]
@@ -36,7 +36,7 @@ const VerifikasiDokumen = (): ReactElement => {
             <div>
                 <input type="file" accept="application/pdf" onChange={handleChange} />
             </div>
-            {result && <div style={{
+            {/* {result && <div style={{
                 width: '400px',
                 backgroundColor: '#555',
                 textAlign: 'start',
@@ -45,7 +45,38 @@ const VerifikasiDokumen = (): ReactElement => {
                 padding: '5px 10px'
             }}>
                 {result}
-            </div>}
+            </div>} */}
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: '10px'
+            }}>
+                {result.map((item: any, index: number) => {
+                    return <div key={index} style={{backgroundColor: 'white', color: 'black', padding: '10px 10px 5px 10px', borderRadius: '5px', border: '1px solid #ddd', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <div style={{textAlign: 'start', width: '100%', display: 'flex', alignItems: 'center', gap: '5px'}}>
+                            <div style={{padding: '5px 10px',  textAlign: 'center', border: '1px solid #ddd', borderRadius: '5px'}}>{item.number}</div> 
+                            <div style={{
+                                backgroundColor: item.validity ? 'green' : 'red',
+                                color: 'white',
+                                width: 'calc(100% - 20px)',
+                                textAlign: 'center',
+                                padding: '5px 10px',
+                                borderRadius: '5px',
+                                fontWeight: 'bold'
+                            }}>{item.validity ? 'Valid' : 'Tidak Valid'}</div>
+                        </div>
+                        <table style={{border: 'none', background: 'none'}}>
+                            <tbody>
+                                <tr><th style={{background: 'none', border: 'none', color: 'black'}}>Penanda tangan</th><td style={{border: 'none'}}>{item.signer}</td></tr>
+                                <tr><th style={{background: 'none', border: 'none', color: 'black'}}>Serial number</th><td style={{border: 'none'}}>{item.serialNumber}</td></tr>
+                                <tr><th style={{background: 'none', border: 'none', color: 'black'}}>Sertifikat oleh</th><td style={{border: 'none'}}>{item.certifiedBy}</td></tr>
+                                <tr><th style={{background: 'none', border: 'none', color: 'black'}}>Tanggal tanda tangan</th><td style={{border: 'none'}}>{item.signedAt}</td></tr>
+                            </tbody>
+                        </table>
+                    </div>
+                })}
+            </div>
         </div>
     </Homepage>
 }
